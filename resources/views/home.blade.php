b19d60a778e30112f59d89ab3428fcb4686af017
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-02-01 20:19:07 +0700

    Commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+
+
+@extends('layouts.main')
+
+@section('title', 'Beranda')
+    
+
+@section('konten')
+    
+    <p>Halo gaes</p>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <script src="https://kit.fontawesome.com/a13d2f4f0a.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+
+    @yield('konten')
+
+    
+    <footer class="bg-slate-700 w-full h-[30vh] text-white">
+        <div class="w-full h-full items-center px-5 flex justify-between">
+            <ul>
+                <li>Fitur Fitur Tersedia</li>
+                <li><a href="">Bernada</a></li>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Kontak Saya</a></li>
+            </ul>
+            <ul>
+                <li>Fitur Fitur Tersedia</li>
+                <li><a href="">Bernada</a></li>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Kontak Saya</a></li>
+            </ul>
+            <ul>
+                <li>Fitur Fitur Tersedia</li>
+                <li><a href="">Bernada</a></li>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Kontak Saya</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
